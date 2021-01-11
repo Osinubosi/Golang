25c0cad1e03f6775cf9fd13eeb1a5048faee153f
Olayinka Yusuf <ekoballs@hotmail.com>
2021-01-11 20:36:40 +0100

    commit init

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/lib/pq"
+	"github.com/subosito/gotenv"
+
+	"github.com/gorilla/mux"
+)
+
+//model book to get or add a new book
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Year   string `json:"year"`
+}
+
+var books []Book
+
+var db *sql.DB
+
+func init() {
+
+	gotenv.Load()
+}
+
+func logFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+
+	pgUrl, err := pq.ParseURL(os.Getenv("ELEPHANTSQL_URL"))
+	logFatal(err)
+
+	log.Println(pgUrl)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func removeBook(w http.ResponseWriter, r *http.Request) {
+
+}
